0ae6c0d2fcb3d11a9535334e41144d5b5e4b6bb0
Dennys Valentina Mosquera Maturin <1006168982@academico.icesi.edu.co>
2019-05-27 12:31:58 -0500

    rappi interface added

--- Graphs/src/application/SignInController.java
+++ Graphs/src/application/SignInController.java
@@ -1,12 +1,25 @@
 package application;
 
+import java.io.IOException;
+
+import org.w3c.dom.events.MouseEvent;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class SignInController {
 	
@@ -26,6 +39,10 @@ public class SignInController {
 	private PasswordField txtContrasenia;
 	@FXML
 	private Button btEntrar;
+	@FXML
+	private Button btSignUp;
+	
+	
 	
     private static final String IDLE_BUTTON_STYLE = "-fx-background-color: transparent;"; 
     private static final String HOVERED_BUTTON_STYLE = "-fx-background-color: -fx-shadow-highlight-color, -fx-outer-border, -fx-inner-border, -fx-body-color;";
@@ -39,8 +56,31 @@ public class SignInController {
 		
 	}
 	
+	@FXML
+	public void nuevaCuenta(ActionEvent e) throws IOException {
+		System.out.println("Nueva cuenta!!");
+		Pane root = (Pane)FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+		root.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+		Scene scene = new Scene(root);
+		Stage pantalla = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		pantalla.setScene(scene);
+		pantalla.show();
+		pantalla.setResizable(false);
+	}
 	
+
 	
+	@FXML
+	public void ventanaPrincipal(ActionEvent e) throws IOException {
+		System.out.println("Rappiii!!");
+		Pane root = (Pane)FXMLLoader.load(getClass().getResource("Rappi.fxml"));
+		root.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+		Scene scene = new Scene(root);
+		Stage pantalla = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		pantalla.setScene(scene);
+		pantalla.show();
+		pantalla.setResizable(false);
+	}
 	
 	
 	/**
--- Graphs/src/application/SignUpController.java
+++ Graphs/src/application/SignUpController.java
@@ -1,10 +1,21 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class SignUpController {
 
@@ -27,4 +38,20 @@ public class SignUpController {
 	@FXML
 	private ImageView imgFondo;
 
+	
+	public void initialize() {
+		
+	}
+	
+	@FXML
+	public void iniciarSesion(ActionEvent e) throws IOException {
+		System.out.println("Inicio de sesión!!");
+		Pane root = (Pane)FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+		root.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+		Scene scene = new Scene(root);
+		Stage pantalla = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		pantalla.setScene(scene);
+		pantalla.show();
+		pantalla.setResizable(false);
+	}
 }
